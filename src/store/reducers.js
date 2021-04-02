d8a6e82b4d58a079d7af56284103c429dd938acc
Maybiz <maxime.julliant@laposte.net>
2021-04-02 07:34:27 +0200

    lecon ok

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,33 +1,35 @@
-import * as actions from './action'
+import { combineReducers } from 'redux';
+import * as actions from './actions';
 
-export const todoReducer = (state, action) => {
+const todos = (state = [], action) => {
     switch (action.type) {
-      case actions.ADD_TODO:
-        return {
-          ...state,
-          todos: [ ...state.todos, action.todo ]
+    case actions.ADD_TODO:
+      return [ ...state, action.todo ];
+
+    case actions.DELETE_TODO:
+      return state.data.filter((t, i) => i !== action.index);
+
+    case actions.TOGGLE_TODO:
+      return state.map((t, i) => {
+        if (i === action.index) {
+          t.done = !t.done;
         }
-      case actions.DELETE_TODO:
-        return {
-          ...state,
-          todos: state.todos.filter((t, i) => i !== action.index)
-        }
-      case actions.SET_FILTER:
-        return {
-          ...state,
-          filter: action.filter
-        }
-      case actions.TOGGLE_TODO:
-        return {
-          ...state,
-          todos: state.todos.map((t, i) => {
-            if (i === action.index) {
-              t.done = !t.done;
-            }
-            return t;
-          })
-        }
-      default:
-        return state
-    }
+        return t;
+      })
+
+    default:
+      return state
   }
+}
+
+const filter = (state = actions.VisibilityFilters.SHOW_ALL, action) => {
+    switch (action.type) {
+    case actions.SET_FILTER:
+      return action.filter;
+    default:
+      return state
+  }
+}
+
+
+export const todoAppReducer = combineReducers({ todos, filter })
